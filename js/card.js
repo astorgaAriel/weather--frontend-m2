3ed60351c8f96b345547f160a85052c0cbeb57a6
ArielAstorga <astorga.ariel.k@gmail.com>
2026-04-07 02:15:54 -0400

    subiendo layout de la app con imagen svg

---
+++ js/card.js
@@ -0,0 +1,19 @@
+const paths = document.querySelectorAll("#mapa path");
+const preview = document.getElementById("preview");
+
+paths.forEach(path => {
+  path.addEventListener("click", () => {
+
+    const bbox = path.getBBox();
+
+    preview.innerHTML = "";
+
+    // Ajusta el viewBox al tamaño exacto del path
+    preview.setAttribute(
+      "viewBox",
+      `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`
+    );
+
+    preview.appendChild(path.cloneNode(true));
+  });
+});
